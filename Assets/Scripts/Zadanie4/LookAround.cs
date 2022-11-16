41afcb81bdd35f0023fe62497a7497b425eec043
dmurawski <dmurawski>
2022-11-16 16:08:00 +0100

    fix zadanie4

--- Assets/Scripts/Zadanie4/LookAround.cs
+++ Assets/Scripts/Zadanie4/LookAround.cs
@@ -10,6 +10,7 @@ public class LookAround : MonoBehaviour
     public Transform player;
     float minX = -90f;
     float maxX = 90f;
+    float xRotation;
     public float sensitivity = 200f;
 
     void Start()
@@ -24,13 +25,18 @@ public class LookAround : MonoBehaviour
         // pobieramy wartoœci dla obu osi ruchu myszy
         float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-        
+
+        xRotation -= mouseYMove;
+        xRotation = Mathf.Clamp(xRotation, minX, maxX);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         player.Rotate(Vector3.up * mouseXMove);
-        mouseXMove = Math.Clamp(mouseXMove, minX, maxX);
+
+        //limit = Math.Clamp(mouseXMove, minX, maxX);
         // a dla osi X obracamy kamerê dla lokalnych koordynatów
         // -mouseYMove aby unikn¹æ ofektu mouse inverse
-        if (mouseXMove >= -90 && mouseXMove <= 90)
-            transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
-
+        //if (limit <= -90 && limit >= 90)
+          //  transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
+    //    player.Rotate(Vector3.up * mouseXMove);
     }
 }
